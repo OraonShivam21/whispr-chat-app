862a542803b9a4f8bfa0deea5982b4e934c21e42
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-06-12 16:17:36 +0530

    added config for various theme settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
-import { closeSnackBar } from "./redux/slicer/app.jsx";
+import { closeSnackBar } from "./redux/slicer/app";
 
 const vertical = "bottom";
 const horizontal = "center";
