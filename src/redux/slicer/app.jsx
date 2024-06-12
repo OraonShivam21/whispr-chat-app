094ccdf9038a1502d18cb109aa49f0d67a24d0b2
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-06-12 13:16:22 +0530

    created redux slicer to open and close snackbar

---
+++ src/redux/slicer/app.jsx
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {},
+  sideBar: {
+    open: false,
+    type: "CONTACT", // can be CONTACT, STARRED, SHARED
+  },
+  isLoggedIn: true,
+  tab: 0, // [0, 1, 2, 3]
+  snackbar: {
+    open: null,
+    severity: null,
+    message: null,
+  },
+  users: [], // all users of app who are not friends and not requested yet
+  all_users: [],
+  friends: [], // all friends
+  friendRequests: [], // all friend requests
+  chat_type: null,
+  room_id: null,
+  call_logs: [],
+};
+
+const slice = createSlice({
+  name: "whispr-chat-application",
+  initialState,
+  reducers: {
+    openSnackBar(state, action) {
+      state.snackbar.open = true;
+      state.snackbar.severity = action.payload.severity;
+      state.snackbar.message = action.payload.message;
+    },
+    closeSnackBar(state) {
+      state.snackbar.open = false;
+      state.snackbar.message = null;
+    },
+  },
+});
+
+export default slice.reducer;
+
+export const closeSnackBar = () => async (dispatch, getState) => {
+  dispatch(slice.actions.closeSnackBar());
+};
