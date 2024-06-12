144ea0becf54bd78d39d87f167877500aa2a634a
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-06-12 16:03:48 +0530

    added palette for toggling between dark and light theme

---
+++ src/theme/index.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+import { useMemo } from "react";
+// @mui
+import { CssBaseline } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider as MUIThemeProvider,
+  StyledEngineProvider,
+} from "@mui/material";
+// hooks
+import useSettings from "../hooks/useSettings";
